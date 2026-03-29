11110e7f0da311c40b98010b053dfb0d9349a1a7
Jivan <jivanmate5263@gmail.com>
2026-03-29 14:19:26 +0530

    CrudTestDone

--- Controller/StockController.cs
+++ Controller/StockController.cs
@@ -23,13 +23,13 @@ namespace api.Controllers
 
         public ActionResult GetAllStocks()
         {
-            var stocks = _context.Stocks.ToList().Select(s=>s.ToStockDto());
+            var stocks = _context.Stocks.ToList().Select(s => s.ToStockDto());
             return Ok(stocks);
         }
 
         [HttpGet("{id}")]
         public ActionResult GetStockById([FromRoute] int id)
-        {  
+        {
             var stock = _context.Stocks.FirstOrDefault(s => s.Id == id);
             if (stock == null)
             {
@@ -44,7 +44,43 @@ namespace api.Controllers
             var stockModel = stockDto.ToSTockFromCreateDto();
             _context.Stocks.Add(stockModel);
             _context.SaveChanges();
-           return CreatedAtAction(nameof(GetStockById), new { id = stockModel.Id }, stockModel.ToStockDto());
+            return CreatedAtAction(nameof(GetStockById), new { id = stockModel.Id }, stockModel.ToStockDto());
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult UpdateStock([FromRoute] int id, [FromBody] UpdateStockRequestDto updateStockRequestDto)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(s => s.Id == id);
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+
+            stockModel.Symbol = updateStockRequestDto.Symbol;
+            stockModel.CompanyName = updateStockRequestDto.CompanyName;
+            stockModel.Purchase = updateStockRequestDto.Purchase;
+            stockModel.LastDiv = updateStockRequestDto.LastDiv;
+            stockModel.Industry = updateStockRequestDto.Industry;
+            stockModel.MarketCap = updateStockRequestDto.MarketCap;
+
+            _context.SaveChanges();
+            return Ok(stockModel.ToStockDto());
+
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult DeleteStock([FromRoute] int id)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(s => s.Id == id);
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+            _context.Stocks.Remove(stockModel);
+            _context.SaveChanges();
+            return NoContent();
         }
     }
 }
